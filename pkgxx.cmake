61fa2f7418adbcb2ba0529e9f55d4338f46cf1fd
Jon Spencer <jon@jonspencer.ca>
2021-10-21 12:15:55 -0700

    better control how pkgxx sets the toolchain

--- pkgxx.cmake
+++ pkgxx.cmake
@@ -15,8 +15,20 @@ if(NOT DEFINED ENV{VCPKG_ROOT})
 	set(ENV{VCPKG_ROOT} ${PKGXX_OUTPUT})
 endif()
 
-if(DEFINED ENV{VCPKG_ROOT} AND NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-  set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
-      CACHE STRING "")
+# decide if pkgxx should control the tool chain by default
+set(DEFAULT_CONTROL TRUE)
+if (DEFINED CMAKE_TOOLCHAIN_FILE)
+	set(DEFAULT_CONTROL FALSE)
 endif()
 
+# set a cache variable to decide if pkgxx controls the tool chain
+set(PKGXX_CONTROL_TOOLCHAIN ${DEFAULT_CONTROL} CACHE BOOL "pkgxx controls the tool chain")
+
+# set the toolchain if pkgxx is in control
+if(DEFINED ENV{VCPKG_ROOT} AND PKGXX_CONTROL_TOOLCHAIN)
+	set(TARGET_TOOLCHAIN "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+	if (NOT CMAKE_TOOLCHAIN_FILE EQUAL TARGET_TOOLCHAIN)
+		set(CMAKE_TOOLCHAIN_FILE ${TARGET_TOOLCHAIN} CACHE STRING "" FORCE)
+		set(Z_VCPKG_ROOT_DIR $ENV{VCPKG_ROOT} CACHE INTERNAL "" FORCE)
+	endif()
+endif()
